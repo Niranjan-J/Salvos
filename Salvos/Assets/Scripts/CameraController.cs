ebeeecae9f2b67905a7801b81b78debbec838bd7
Niranjan-J <cse160001026@iiti.ac.in>
2018-03-13 17:31:10 +0530

    Commit progress until now.

---
+++ Salvos/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private Vector3 offset;
+	private void Start() {
+		offset= transform.position-player.transform.position;
+	}
+	private void LateUpdate() {
+		transform.rotation=player.transform.rotation;
+		transform.position=player.transform.position+player.transform.rotation*offset;
+	}
+}
+
