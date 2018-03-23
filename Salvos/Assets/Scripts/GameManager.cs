104445de662ecae317384c2bcf5d12869405d7aa
Amit Meena <cse160001004@iiti.ac.in>
2018-03-23 14:05:12 +0530

    Score Addded to quiz

--- Salvos/Assets/Scripts/GameManager.cs
+++ Salvos/Assets/Scripts/GameManager.cs
@@ -9,7 +9,9 @@ using UnityEngine.EventSystems;
 public class GameManager : MonoBehaviour {
     public Question[] questions;
     private static List<Question> unansweredQuestions;
-    
+    public Text score;
+    private static int i = 0;
+    private static int j = 0;
     private Question currentQuestion;
     [SerializeField]
     private Text factText;
@@ -36,11 +38,16 @@ public class GameManager : MonoBehaviour {
     */
     void Start()
     {
+        score.text = "Score:  " + i.ToString();
         if(unansweredQuestions == null || unansweredQuestions.Count == 0)
         {
             unansweredQuestions = questions.ToList<Question>();
         }
-        
+        j++;
+        if(j==4)
+        {
+            SceneManager.LoadScene(4);
+        }
         SetCurrentQuestion ();
         
     }
@@ -74,6 +81,7 @@ IEnumerator NextQuestion ()
         
         if (currentQuestion.ans == 0)
         {
+            i++;
             animator.SetTrigger("T_OP0");
         }
         else
@@ -86,6 +94,7 @@ IEnumerator NextQuestion ()
     {
         if (currentQuestion.ans == 1)
         {
+            i++;
             animator.SetTrigger("T_OP1");
         }
         else
@@ -98,6 +107,7 @@ IEnumerator NextQuestion ()
     {
         if (currentQuestion.ans == 2)
         {
+            i++;
             animator.SetTrigger("T_OP2");
         }
         else
@@ -110,6 +120,7 @@ IEnumerator NextQuestion ()
     {
         if (currentQuestion.ans == 3)
         {
+            i++;
             animator.SetTrigger("T_OP3");
         }
         else
