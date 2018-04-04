d5edaec2e6af247f126a635ad6b230c6ed86d986
Niranjan-J <cse160001026@iiti.ac.in>
2018-04-05 00:33:00 +0530

    Mission 2 improvements

---
+++ Salvos/Assets/Scripts/RocksCollisionHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RocksCollisionHandler : MonoBehaviour {
+	public GameObject p;
+	public Text mt;
+	private void OnTriggerEnter(Collider other) {
+		if(other.CompareTag("Player")){
+			mt.text="GAME OVER.\nHint: Stay away from the rocks and boulders. They are dangerous.";
+		}
+		p.SetActive(true);
+	}
+}
